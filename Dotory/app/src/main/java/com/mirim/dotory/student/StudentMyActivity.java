91fafb2d8e2cd1896c565e40153507d2a9a2b7ba
lyj310 <yoenjii310@gmail.com>
2021-04-28 11:49:43 +0900

    create student's pages

---
+++ Dotory/app/src/main/java/com/mirim/dotory/student/StudentMyActivity.java
@@ -0,0 +1,16 @@
+package com.mirim.dotory.student;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mirim.dotory.R;
+
+public class StudentMyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_my);
+    }
+}
