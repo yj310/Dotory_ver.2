afdc6de12c7513aa0e431351c6813c61657e3926
lyj310 <yoenjii310@gmail.com>
2021-06-21 03:01:55 +0900

    add manager login function

--- Dotory/app/src/main/java/com/mirim/dotory/manager/ManagerLoginActivity.java
+++ Dotory/app/src/main/java/com/mirim/dotory/manager/ManagerLoginActivity.java
@@ -2,23 +2,25 @@ package com.mirim.dotory.manager;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.mirim.dotory.LoginActivity;
 import com.mirim.dotory.R;
-import com.mirim.dotory.student.StudentBoardActivity;
 
 public class ManagerLoginActivity extends AppCompatActivity {
+
+    private TextView tv_id;
+    private TextView tv_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +29,9 @@ public class ManagerLoginActivity extends AppCompatActivity {
 
         findViewById(R.id.btn_back).setOnClickListener(onClickListener);
         findViewById(R.id.btn_login).setOnClickListener(onClickListener);
+
+        tv_id = findViewById(R.id.input_id);
+        tv_password = findViewById(R.id.input_password);
     }
 
     View.OnClickListener onClickListener = new View.OnClickListener() {
@@ -45,48 +50,43 @@ public class ManagerLoginActivity extends AppCompatActivity {
     };
 
 
-    public void LogIn() {/*
-        final String password = ((EditText) findViewById(R.id.input_password)).getText().toString();
+    private void LogIn() {
+        if (tv_id.getText().length() > 0 && tv_password.getText().length() > 0 ) {
+            FirebaseDatabase database = FirebaseDatabase.getInstance();;
+            DatabaseReference databaseReference = database.getReference("ManagerUser");
 
-        database = FirebaseDatabase.getInstance();
-
-        databaseReference = database.getReference("ManagerUser");
-        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                // 파이어베이스의 데이터를 받아오는 곳
-
-                if(password.length() > 0) {
-                    if (password.equals(dataSnapshot.child("password").getValue().toString())) {
-                        Intent intent = new Intent(ManagerLoginActivity.this, ManagerBoardActivity.class);
-                        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.GINGERBREAD_MR1) {
-                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                        } else {
-                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    // 파이어베이스의 데이터를 받아오는 곳
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        if(snapshot.getKey().equals(tv_id.getText().toString())) {
+                            if(snapshot.child("password").getValue().equals(tv_password.getText().toString())) {
+                                Intent intent = new Intent(ManagerLoginActivity.this, ManagerBoardActivity.class);
+                                ActivityCompat.finishAffinity(ManagerLoginActivity.this);
+                                startActivity(intent);
+                                finish();
+                                return;
+                            } else {
+                                Toast.makeText(ManagerLoginActivity.this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
                         }
-                        startActivity(intent);
-                    } else {
-                        Toast.makeText(ManagerLoginActivity.this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
                     }
-                } else {
-                    Toast.makeText(ManagerLoginActivity.this, "비밀번호를 입력해주십시오.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(ManagerLoginActivity.this, "존재하지 않는 아이디입니다.", Toast.LENGTH_SHORT).show();
+
+
+                };
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    // DB를 가져오던 중 에러 발생 시
+                    Toast.makeText(ManagerLoginActivity.this, error.toException().toString(), Toast.LENGTH_SHORT).show();
                 }
-
-            };
-
-
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                // DB를 가져오던 중 에러 발생 시
-                Toast.makeText(ManagerLoginActivity.this, error.toException().toString(), Toast.LENGTH_SHORT).show();
-            }
-        });*/
-
-        Intent intent = new Intent(ManagerLoginActivity.this, ManagerBoardActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivity(intent);
-        finish();
+            });
+        } else {
+            Toast.makeText(this, "입력되지 않은 항목이 존재합니다. ", Toast.LENGTH_SHORT).show();
+        }
+        
     }
 
 }
