b1847cd91d1401249edf80dceed6713ea73b85d1
yj310 <yoenjii310@gmail.com>
2021-06-21 16:12:57 +0900

    add Student GoOut function

---
+++ Dotory/app/src/main/java/com/mirim/dotory/GoOutInfo.java
@@ -0,0 +1,91 @@
+package com.mirim.dotory;
+
+public class GoOutInfo {
+
+    private String enter_time;
+    private String expect_time;
+    private String go_out_time;
+    private String name;
+    private String place;
+    private String reason;
+    private int room;
+    private String state;
+
+    public GoOutInfo() {
+    }
+
+    public GoOutInfo(String expect_time, String go_out_time, String name, String place, String reason, int room) {
+        this.enter_time = "";
+        this.expect_time = expect_time;
+        this.go_out_time = go_out_time;
+        this.name = name;
+        this.place = place;
+        this.reason = reason;
+        this.room = room;
+        this.state = "외출중";
+    }
+
+    public String getEnter_time() {
+        return enter_time;
+    }
+
+    public void setEnter_time(String enter_time) {
+        this.enter_time = enter_time;
+    }
+
+    public String getExpect_time() {
+        return expect_time;
+    }
+
+    public void setExpect_time(String expect_time) {
+        this.expect_time = expect_time;
+    }
+
+    public String getGo_out_time() {
+        return go_out_time;
+    }
+
+    public void setGo_out_time(String go_out_time) {
+        this.go_out_time = go_out_time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public int getRoom() {
+        return room;
+    }
+
+    public void setRoom(int room) {
+        this.room = room;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
--- Dotory/app/src/main/java/com/mirim/dotory/student/StudentGoOutActivity.java
+++ Dotory/app/src/main/java/com/mirim/dotory/student/StudentGoOutActivity.java
@@ -1,5 +1,6 @@
 package com.mirim.dotory.student;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,19 +10,47 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.mirim.dotory.GoOutInfo;
 import com.mirim.dotory.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class StudentGoOutActivity extends AppCompatActivity {
 
-    TextView text_hour;
-    TextView text_minute;
-    TextView text_AMPM;
+    TextView txt_hour;
+    TextView txt_minute;
+    TextView txt_AMPM;
+
+    TextView txt_now_time;
+    TextView txt_now_ampm;
+    TextView txt_today;
+    TextView txt_state;
+    EditText input_reason;
+    EditText input_place;
+    TextView btn_submit;
+
+    LinearLayout btn_time_select;
 
     private String email;
+    private String key;
+    private String today;
+    private int room;
+    private String name;
+    
+    private String startTime;
+    private String endTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,11 +67,51 @@ public class StudentGoOutActivity extends AppCompatActivity {
         findViewById(R.id.btn_bottombar_my).setOnClickListener(onClickListener);
         findViewById(R.id.btn_titlebar_goout).setOnClickListener(onClickListener);
         findViewById(R.id.btn_titlebar_stayout).setOnClickListener(onClickListener);
-        findViewById(R.id.btn_time_select).setOnClickListener(onClickListener);
+        findViewById(R.id.btn_submit).setOnClickListener(onClickListener);
 
-        text_hour = findViewById(R.id.text_select_hour);
-        text_minute = findViewById(R.id.text_select_minute);
-        text_AMPM = findViewById(R.id.text_select_AMPM);
+        btn_time_select = findViewById(R.id.btn_time_select);
+        btn_time_select.setOnClickListener(onClickListener);
+
+        txt_hour = findViewById(R.id.text_select_hour);
+        txt_minute = findViewById(R.id.text_select_minute);
+        txt_AMPM = findViewById(R.id.text_select_AMPM);
+
+        txt_now_time = findViewById(R.id.txt_now_time);
+        txt_now_ampm = findViewById(R.id.txt_now_ampm);
+        txt_today = findViewById(R.id.txt_today);
+        txt_state = findViewById(R.id.txt_state);
+
+        input_reason = findViewById(R.id.input_reason);
+        input_place = findViewById(R.id.input_place);
+
+
+        final Date now = new Date();
+
+        SimpleDateFormat formatter = new SimpleDateFormat("MM월 dd일 E요일");
+        txt_today.setText(formatter.format(now));
+
+        formatter = new SimpleDateFormat("hh:mm");
+        txt_now_time.setText(formatter.format(now));
+
+
+        formatter = new SimpleDateFormat("a");
+        if(formatter.format(now).equals("오전")) {
+            txt_now_ampm.setText("AM");
+            txt_AMPM.setText("AM");
+        } else {
+            txt_now_ampm.setText("PM");
+            txt_AMPM.setText("PM");
+        }
+
+        formatter = new SimpleDateFormat("hh");
+        txt_hour.setText(formatter.format(now));
+        formatter = new SimpleDateFormat("mm");
+        txt_minute.setText(formatter.format(now));
+
+        formatter = new SimpleDateFormat("yyyy-MM-dd");
+        today = formatter.format(now);
+
+        getState();
 
     }
 
@@ -102,12 +171,12 @@ public class StudentGoOutActivity extends AppCompatActivity {
                     break;
                 case R.id.btn_time_select:
 
-                    int hour = getIntInText(text_hour);
-                    int minute = getIntInText(text_minute);
+                    int hour = getIntInText(txt_hour);
+                    int minute = getIntInText(txt_minute);
                     if(hour == 12) {
                         hour = 0;
                     }
-                    if(text_AMPM.getText().equals("PM")) {
+                    if(txt_AMPM.getText().equals("PM")) {
                         hour += 12;
                     }
 
@@ -118,31 +187,144 @@ public class StudentGoOutActivity extends AppCompatActivity {
 
                             // AMPM
                             if(hour < 12) {
-                                text_AMPM.setText("AM");
+                                txt_AMPM.setText("AM");
                             } else {
-                                text_AMPM.setText("PM");
+                                txt_AMPM.setText("PM");
                             }
 
                             // hour
                             int result_hour = hour % 12;
                             if(result_hour == 0) result_hour = 12;
-                            text_hour.setText(String.valueOf(result_hour));
+                            txt_hour.setText(String.valueOf(result_hour));
 
                             // minute
                             if(minute < 10) {
-                                text_minute.setText("0" + String.valueOf(minute));
+                                txt_minute.setText("0" + String.valueOf(minute));
                             } else {
-                                text_minute.setText(String.valueOf(minute));
+                                txt_minute.setText(String.valueOf(minute));
                             }
                         }
                     }, hour, minute, false);
                     timePickerDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
                     timePickerDialog.show();
                     break;
+                case R.id.btn_submit:
+                    addGoOutInfo();
+                    break;
 
             }
         }
     };
+
+    private void getState() {
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference;
+
+
+        // 외출 시간 불러오기
+        databaseReference = database.getReference("GoOut");
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                
+                if(!dataSnapshot.child(today).child("time").child("start").getValue().toString().equals("")) {
+                    // 외박 가능 시간 설정 있음
+                    startTime = dataSnapshot.child(today).child("time").child("start").getValue().toString();
+                    endTime = dataSnapshot.child(today).child("time").child("end").getValue().toString();
+                    //text_go_out_time.setText(first_time + "      " + second_time);
+                } else {
+                    // 외박 가능 시간 설정 없음
+                }
+                
+
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // DB를 가져오던 중 에러 발생 시
+                Toast.makeText(StudentGoOutActivity.this, error.toException().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        databaseReference = database.getReference("StudentUser");
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // 파이어베이스의 데이터를 받아오는 곳
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    StudentUser studentUser = snapshot.getValue(StudentUser.class);
+                    if(studentUser.getEmail().equals(email)) {
+                        name = studentUser.getName();
+                        room = studentUser.getRoom();
+
+                        key = studentUser.getRoom() + email.split("@")[0];
+
+                        // get my state
+                        DatabaseReference databaseReference = database.getReference("GoOut/"+ today +"/student");
+                        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                    if(snapshot.getKey().equals(key)) {
+                                        // 이미 외출 정보가 있음
+                                        GoOutInfo goOutInfo = snapshot.getValue(GoOutInfo.class);
+
+                                        txt_state.setText(goOutInfo.getState());
+                                        input_reason.setText(goOutInfo.getReason());
+                                        input_place.setText(goOutInfo.getPlace());
+                                        String expect_time = goOutInfo.getExpect_time();
+                                        txt_hour.setText(expect_time.split(":")[0]);
+                                        txt_minute.setText(expect_time.split(":")[1]);
+                                        input_reason.setFocusable(false);
+                                        input_reason.setClickable(false);
+                                        input_place.setFocusable(false);
+                                        input_place.setClickable(false);
+                                        btn_time_select.setFocusable(false);
+                                        btn_time_select.setClickable(false);
+
+                                        return;
+                                    }
+                                }
+                                // 외출 정보 없음
+                                Date now = new Date();
+                                SimpleDateFormat formatter = new SimpleDateFormat("HH:mm");
+                                String now_time = formatter.format(now);
+
+                                if((Integer.parseInt(first_time.split(":")[0]) < Integer.parseInt(now_time.split(":")[0])
+                                        || (Integer.parseInt(first_time.split(":")[0]) == Integer.parseInt(now_time.split(":")[0])
+                                        && Integer.parseInt(first_time.split(":")[1]) <= Integer.parseInt(now_time.split(":")[1])))
+                                        && (Integer.parseInt(second_time.split(":")[0]) > Integer.parseInt(now_time.split(":")[0])
+                                        || (Integer.parseInt(second_time.split(":")[0]) == Integer.parseInt(now_time.split(":")[0])
+                                        && Integer.parseInt(second_time.split(":")[1]) <= Integer.parseInt(now_time.split(":")[1])))) {
+
+                                    text_state.setText("외출 가능");
+                                }
+
+                            }
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+                                // DB를 가져오던 중 에러 발생 시
+                                Toast.makeText(StudentAttendanceGoOutActivity.this, error.toException().toString(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        break;
+                    }
+
+                }
+            };
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // DB를 가져오던 중 에러 발생 시
+                Toast.makeText(StudentAttendanceGoOutActivity.this, error.toException().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    private void addGoOutInfo() {
+
+    }
 
     public int getIntInText(TextView view) {
         return Integer.parseInt(view.getText().toString());
